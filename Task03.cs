3517e1f8a1ad52cfbeed50390bd169b4bfb7492d
Eugene <eugene132003@list.ru>
2023-03-24 20:17:31 +0300

    Task 3. Part 1

--- Task03.cs
+++ Task03.cs
@@ -2,6 +2,11 @@ class Task_03
 {
     public static void Main()
     {
-        
+        int a=4;
+        if (a%2==0) {
+            Console.Write("Да");
+        }else{
+            Console.Write("Нет");
+        }
     }
 }
