e08cf4302d071149f100293b6ee7ace09c3f2be3
Eugene <eugene132003@list.ru>
2023-03-24 20:30:02 +0300

    Task 4. Part 1

--- Task04.cs
+++ Task04.cs
@@ -2,7 +2,20 @@ class Task_04
 {
     public static void Main()
     {
-
-
+        int a = 5;
+        for (int i = 1; i < a + 1; i++)
+        {
+            if (i % 2 == 0)
+            {
+                if (i+1<a)
+                {
+                    Console.Write(i + ", ");
+                }
+                else
+                {
+                    Console.Write(i);
+                }
+            }
+        }
     }
 }
