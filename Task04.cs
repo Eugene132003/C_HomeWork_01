c7215bc0d34229087b0df465af6504fbb543381c
Eugene <eugene132003@list.ru>
2023-03-24 20:30:32 +0300

    Task 4. Part 2

--- Task04.cs
+++ Task04.cs
@@ -2,7 +2,8 @@ class Task_04
 {
     public static void Main()
     {
-        int a = 5;
+        //int a = 5;
+        int a = 8;
         for (int i = 1; i < a + 1; i++)
         {
             if (i % 2 == 0)
