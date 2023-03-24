b26e5a37f10ba09581e05f253c7cdca49aca9767
Eugene <eugene132003@list.ru>
2023-03-24 19:52:50 +0300

    Task 2. Part 3

--- Task02.cs
+++ Task02.cs
@@ -11,7 +11,16 @@ class Task_02
 
         Console.Write("max = " + max2);*/
 
-                int a2 = 44, b2 = 5, c2 = 78;
+        /*int a2 = 44, b2 = 5, c2 = 78;
+        int max2 = a2;
+
+        if (a2 > max2) max2 = a2;
+        if (b2 > max2) max2 = b2;
+        if (c2 > max2) max2 = c2;
+
+        Console.Write("max = " + max2);*/
+
+        int a2 = 22, b2 = 3, c2 = 9;
         int max2 = a2;
 
         if (a2 > max2) max2 = a2;
